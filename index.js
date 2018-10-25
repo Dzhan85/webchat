992989f06cd61ca6c8bd19abf8d4eace6376f568
Dzhan85 <atakurban@gmail.com>
2018-10-25 16:31:58 +0300

    added logging

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-let express = require('express');
-let socket = require('socket.io');
+let express = require('express')
+let socket = require('socket.io')
 
 //App setup
 
-let app = express();
+let app = express()
 
 let server = app.listen(4000, () => {
-  console.log('listening to requests on port 4000');
+  console.log('listening to requests on port 4000')
 });
 
 //static files
 
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 //socket setup
 
-let io = socket(server);
+let io = socket(server)
 
 io.on('connection', (socket) => {
   console.log('made socket connection', socket.id)
   socket.on('chat', (data) => {
-    io.sockets.emit('chat', data);
+    console.log('emiting data', data)
+    io.sockets.emit('chat', data)
   })
 });
